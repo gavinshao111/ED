d5d45c776faeda4b04a519f68b14c8dd8f0180e6
gavinshao111 <yiyecheer@163.com>
2017-01-19 13:38:17 +0800

    update startupInDebugMode.sh

--- EasyDarwin/startUpInDebug.sh
+++ EasyDarwin/startUpInDebug.sh
@@ -6,4 +6,4 @@
 # Created on 2017-1-19, 11:31:33
 #
 date > Logs/running.log
-./debug/easydarwin -c WinNTSupport/easydarwin.xml -d >> Logs/running_$(date -d "today" +"%Y%m%d_%H%M%S").log 2>Logs/stderr_$(date -d "today" +"%Y%m%d_%H%M%S").log &
+./Debug/easydarwin -c WinNTSupport/easydarwin.xml -d >> Logs/running_$(date -d "today" +"%Y%m%d_%H%M%S").log 2>Logs/stderr_$(date -d "today" +"%Y%m%d_%H%M%S").log &
