8c80ba8cd79c05ac748e4ee869ccdf80563b9e9d
gavinshao111 <yiyecheer@163.com>
2017-08-09 17:31:28 +0800

    修复重启失败的bug

--- EasyDarwin/restart.sh
+++ EasyDarwin/restart.sh
@@ -3,13 +3,33 @@
 target=./Debug/easydarwin
 startup_script=startUpInDebug.sh
 
-hoursTo2clcok=$[26 - $(date +%H)]
-sleep ${hoursTo2clcok}h
-while :
-do
+restart(){
     if [ $(ps -ef | grep "$target" | grep -v "grep" | wc -l) -eq 1 ]; then
         kill $(ps -ef | grep "$target" | grep -v "grep" | awk '{print $2}')
+        if [ $? -eq 0 ]; then
+            echo killed
+        else
+            echo kill fail
+            exit 1
+        fi
     fi
+
+    # 虽然上面kill已经返回0，但是此时进程没有退出，sleep 1s等进程退出
+    sleep 1
+
     sh $startup_script
+    echo restart done $(date)
+}
+
+hoursTo2clcok=$[26 - $(date +%H)]
+#timeSleep=$[60 - $(date +%M)]
+
+#restart
+sleep ${hoursTo2clcok}h
+#sleep ${timeSleep}m
+
+while :
+do
+    restart
     sleep 24h
 done
